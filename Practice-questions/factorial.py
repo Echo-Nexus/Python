67a84d52ac95c7d9328098c21ff71a49c5deedd2
Echo-Nexus <vxattitude@gmail.com>
2026-02-02 23:04:09 +0545

    Added the factorial.py to Pratice-questions

---
+++ Practice-questions/factorial.py
@@ -0,0 +1,11 @@
+num = int(input("Enter a number to compute its factorial : "))
+factorial = 1
+
+if num < 0 :
+  print("Sorry, factorial does not exist for negative numbers")
+elif num == 0 or num == 1 :
+  print("The factorial of 0 is 1")
+else :
+  for i in range (2, num + 1) :
+    factorial = factorial * i
+print("The factorial of", num, "is", factorial)
